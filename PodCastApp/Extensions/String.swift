9be738834b5caa8787ce6b076c347bcaa9ccdcd2
SherifZekryWebsites <prosherifzekry@gmail.com>
2018-10-04 00:16:23 +0200

    adding pod files as well to make it ready to work rightaway

---
+++ PodCastApp/Extensions/String.swift
@@ -0,0 +1,17 @@
+//
+//  String.swift
+//  PodCastApp
+//
+//  Created by Sherif  Wagih on 9/28/18.
+//  Copyright © 2018 Sherif  Wagih. All rights reserved.
+//
+
+import UIKit
+extension String
+{
+   /* func toSecureHTTPS() -> String
+    {
+        return self.contains("https") ? self : self.replacingOccurrences(of: "http", with: "https")
+    }*/
+    
+}
